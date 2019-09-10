aeb4f078a9d64d65f43c7d926a1517b062ac1795
Vardhaman Surana <maans406@gmail.com>
2019-09-10 18:55:58 +0530

    second commit

--- pkg/queue/queue.go
+++ pkg/queue/queue.go
@@ -61,6 +61,7 @@ func rConnect() *amqp.Connection{
 	if err!=nil{
 		return nil
 	}else {
+		fmt.Printf("%v",env.URL)
 		conn, err := amqp.Dial(env.URL)
 		if err != nil {
 			fmt.Printf("trying to reconnect")
