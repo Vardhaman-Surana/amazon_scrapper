c370b9385b2953ca6ef39061a2bbb3e72e1d0421
Vardhaman Surana <maans406@gmail.com>
2019-09-11 14:14:08 +0530

    using alpine image for images

---
+++ cmd/CreateArchives/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/vds/amazon_scrapper/pkg/database/mysql_orp"
+	"log"
+	"time"
+)
+
+func main(){
+	log.Println("*********************************")
+	log.Println("Initializing  Database")
+	log.Println("*********************************")
+	db,err:=mysql_orp.NewDBmap()
+	if err!=nil{
+		fmt.Printf("error initiating DB map: %v",err)
+		return
+	}
+	log.Println("*********************************")
+	log.Println("Database Connected")
+	log.Println("*********************************")
+	now:=time.Now().Unix()
+	db.Exec("update products set deleted=2 where ?-updated > 60",now)
+}
